aec001f7b28cbba64e1b4a2aac1df0887d6cd928
Roshan George <roshangeorge93@gmail.com>
2017-03-20 12:32:13 +0530

    Adding support for physical folders

--- wagtaildocs/migrations/0008_document_folder.py
+++ wagtaildocs/migrations/0008_document_folder.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import wagtail.wagtaildocs.models
 
 
 class Migration(migrations.Migration):
@@ -14,11 +15,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='DocumentFolder',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
-                ('title', models.CharField(verbose_name='title', max_length=255)),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('title', models.CharField(max_length=255, verbose_name='title')),
                 ('created_at', models.DateTimeField(verbose_name='created at', db_index=True, auto_now_add=True)),
+                ('path', models.TextField(blank=True)),
                 ('folder', models.ForeignKey(to='wagtaildocs.DocumentFolder', null=True)),
             ],
+        ),
+        migrations.AlterField(
+            model_name='document',
+            name='file',
+            field=models.FileField(upload_to=wagtail.wagtaildocs.models.get_upload_to, verbose_name='file'),
         ),
         migrations.AddField(
             model_name='document',
--- wagtaildocs/models.py
+++ wagtaildocs/models.py
@@ -1,10 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 
+import os
+import shutil
 import os.path
+from unidecode import unidecode
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch import Signal
@@ -18,6 +22,19 @@ from wagtail.wagtailcore.models import CollectionMember
 from wagtail.wagtailsearch import index
 from wagtail.wagtailsearch.queryset import SearchableQuerySetMixin
 
+DOCUMENTS_FOLDER_NAME = 'documents'
+
+
+def get_upload_to(instance, filename):
+    """
+    Obtain a valid upload path for the document.
+
+    This needs to be a module-level function so that it can be referenced within migrations,
+    but simply delegates to the `get_upload_to` method of the instance, so that AbstractDocument
+    subclasses can override it.
+    """
+    return instance.get_upload_to(filename)
+
 
 class DocumentQuerySet(SearchableQuerySetMixin, models.QuerySet):
     pass
@@ -27,6 +44,7 @@ class DocumentFolder(models.Model):
     folder = models.ForeignKey('self', null=True)   # Null value would mean it was in root image folder
     title = models.CharField(max_length=255, verbose_name=_('title'))
     created_at = models.DateTimeField(verbose_name=_('created at'), auto_now_add=True, db_index=True)
+    path = models.TextField(blank=True)
 
     admin_form_fields = (
         'title',
@@ -48,7 +66,92 @@ class DocumentFolder(models.Model):
         return folder_breadcrumb
 
     def get_subfolders(self):
-        return DocumentFolder.objects.filter(folder = self)
+        return DocumentFolder.objects.filter(folder=self)
+
+    def save(self, *args, **kwargs):
+        # do a unidecode in the title and then
+        # replace non-ascii characters in title with _ , to sidestep issues with filesystem encoding
+        unicoded_title = "".join((i if ord(i) < 128 else '_') for i in unidecode(self.title))
+
+        parent_folder = self.folder
+        if parent_folder:
+            self.path = os.path.join(parent_folder.path, unicoded_title)
+        else:
+            self.path = os.path.join(DOCUMENTS_FOLDER_NAME, unicoded_title)
+
+        if self.pk is None:
+            super(DocumentFolder, self).save()
+            # Create the folder
+            os.makedirs(self.get_complete_path())
+        else:
+            if 'rename' in kwargs and not kwargs['rename']:
+                # In case of sub folders, only the DB needs to updated
+                # The physical path would be updated by the parent folder
+                super(DocumentFolder, self).save()
+            else:
+                current_path = DocumentFolder.objects.get(pk=self.pk).get_complete_path()
+                super(DocumentFolder, self).save()
+                # Rename the folder
+                os.rename(current_path, self.get_complete_path())
+
+            # Update the documents' paths
+            Document = get_document_model()
+            documents = Document.objects.filter(folder=self)
+            for document in documents:
+                document.file.name = os.path.join(self.path, document.filename)
+                document.save()
+
+            # Update the sub_folders' path
+            for sub_folder in self.get_subfolders():
+                sub_folder.save(rename=False)
+
+    def delete(self, *args, **kwargs):
+        # Recursively delete the sub folders
+        for sub_folder in self.get_subfolders():
+            sub_folder.delete()
+
+        # Delete the documents
+        Document = get_document_model()
+        documents = Document.objects.filter(folder=self)
+        for document in documents:
+            document.delete()
+
+        try:
+            # Delete the physical folder
+            shutil.rmtree(self.get_complete_path())
+        except FileNotFoundError:
+            pass
+
+        super(DocumentFolder, self).delete()
+
+    def validate_folder(self):
+        """Validates whether a folder can be created.
+        Performs two types of validation:
+        1. Checks if a DB entry is present.
+        2. Checks if a physical folder exists in the system."""
+
+        unicoded_title = "".join((i if ord(i) < 128 else '_') for i in unidecode(self.title))
+        parent_folder = self.folder
+
+        if parent_folder:
+            if DocumentFolder.objects.filter(folder=parent_folder, title=self.title).count() > 0:
+                raise ValidationError("Folder exists in the DB!", code='db')
+            folder_path = os.path.join(settings.MEDIA_ROOT, parent_folder.path, unicoded_title)
+            if os.path.isdir(folder_path):
+                raise ValidationError("Folder exists in the OS!", code='os')
+        else:
+            if DocumentFolder.objects.filter(folder__isnull=True, title=self.title).count() > 0:
+                raise ValidationError("Folder exists in the DB!", code='db')
+            folder_path = os.path.join(settings.MEDIA_ROOT, DOCUMENTS_FOLDER_NAME, unicoded_title)
+            if os.path.isdir(folder_path):
+                raise ValidationError("Folder exists in the OS!", code='os')
+
+    def get_complete_path(self):
+        return os.path.join(settings.MEDIA_ROOT, self.path)
+
+    def __str__(self):
+        return self.title
+
 
 
 def get_folder_model():
@@ -57,9 +160,9 @@ def get_folder_model():
 
 @python_2_unicode_compatible
 class AbstractDocument(CollectionMember, index.Indexed, models.Model):
-    folder = models.ForeignKey(DocumentFolder, null = True, blank = True) # Null value would be root document folder
+    folder = models.ForeignKey(DocumentFolder, null=True, blank=True)  # Null value would be root document folder
     title = models.CharField(max_length=255, verbose_name=_('title'))
-    file = models.FileField(upload_to='documents', verbose_name=_('file'))
+    file = models.FileField(upload_to=get_upload_to, verbose_name=_('file'))
     created_at = models.DateTimeField(verbose_name=_('created at'), auto_now_add=True)
     uploaded_by_user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -84,6 +187,30 @@ class AbstractDocument(CollectionMember, index.Indexed, models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_upload_to(self, filename):
+        filename = self.file.field.storage.get_valid_name(filename)
+
+        # do a unidecode in the filename and then
+        # replace non-ascii characters in filename with _ , to sidestep issues with filesystem encoding
+        filename = "".join((i if ord(i) < 128 else '_') for i in unidecode(filename))
+
+        # Truncate filename so it fits in the 100 character limit
+        # https://code.djangoproject.com/ticket/9893
+        if self.folder:
+            full_path = os.path.join(self.folder.path, filename)
+        else:
+            full_path = os.path.join(DOCUMENTS_FOLDER_NAME, filename)
+
+        if len(full_path) >= 95:
+            chars_to_trim = len(full_path) - 94
+            prefix, extension = os.path.splitext(filename)
+            filename = prefix[:-chars_to_trim] + extension
+            if self.folder:
+                full_path = os.path.join(self.folder.path, filename)
+            else:
+                full_path = os.path.join(DOCUMENTS_FOLDER_NAME, filename)
+        return full_path
 
     @property
     def filename(self):
