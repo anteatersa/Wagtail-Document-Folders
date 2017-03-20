1ba61aed5c061345af82b7f9a2f0ef782d813ff6
Roshan George <roshangeorge93@gmail.com>
2017-03-20 12:34:23 +0530

    Modification for physical folders

--- wagtaildocs/views/folders.py
+++ wagtaildocs/views/folders.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.exceptions import ValidationError
 
 from wagtail.wagtailadmin.utils import PermissionPolicyChecker
-
 from wagtail.wagtaildocs.models import get_folder_model, get_document_model
 from wagtail.wagtaildocs.forms import get_folder_form
 from wagtail.wagtaildocs.permissions import permission_policy
 
 permission_checker = PermissionPolicyChecker(permission_policy)
+DocumentFolder = get_folder_model()
+Document = get_document_model()
+DocumentFolderForm = get_folder_form(DocumentFolder)
 
 
 @permission_checker.require('add')
 def add(request, add_to_folder=False):
-    DocumentFolder = get_folder_model()
-    DocumentFolderForm = get_folder_form(DocumentFolder)
 
     parent_folder = False
     if add_to_folder:
@@ -21,40 +22,32 @@ def add(request, add_to_folder=False):
     if request.method == 'POST':
         # Build a form for validation
         form = DocumentFolderForm(request.POST)
+        error = True
 
         if form.is_valid():
-            # TODO - Check for clashing filenames
             error = False
 
+            folder = DocumentFolder(
+                title=form.cleaned_data['title'].strip()
+            )
             if parent_folder:
-                if DocumentFolder.objects.filter(folder=parent_folder, title=form.cleaned_data['title'].strip()).count() > 0:
-                    error = True
-                    form._errors['title'] = "Folder already exists"
-            else:
-                if DocumentFolder.objects.filter(folder__isnull=True, title=form.cleaned_data['title'].strip()).count() > 0:
-                    error = True
-                    form._errors['title'] = "Folder already exists"
+                folder.folder = parent_folder
 
-            if not error:
+            try:
+                # Check if the folder is present in the DB or physically present in the OS
+                folder.validate_folder()
+            except ValidationError as e:
+                error = True
+                form._errors['title'] = e.message
+            else:
                 # Save folder
-                folder = DocumentFolder(
-                    title=form.cleaned_data['title'].strip()
-                )
-                if parent_folder:
-                    folder.folder = parent_folder
                 folder.save()
 
-                # Success! Send back to index or document specific folder
-                response = redirect('wagtaildocs:index')
-                response['Location'] += '?folder={0}'.format(folder.id)
-                return response
-            else:
-                return render(request, 'wagtaildocs/folder/add.html', {
-                    'error_message': 'Error adding folder',
-                    'help_text': '',
-                    'parent_folder': parent_folder,
-                    'form': form,
-                })
+        if not error:
+            # Success! Send back to index or document specific folder
+            response = redirect('wagtaildocs:index')
+            response['Location'] += '?folder={0}'.format(folder.id)
+            return response
         else:
             # Validation error
             return render(request, 'wagtaildocs/folder/add.html', {
@@ -63,21 +56,19 @@ def add(request, add_to_folder=False):
                 'parent_folder': parent_folder,
                 'form': form,
             })
+
     else:
         form = DocumentFolderForm()
 
-    return render(request, 'wagtaildocs/folder/add.html', {
-        'help_text': '',
-        'parent_folder': parent_folder,
-        'form': form,
-    })
+        return render(request, 'wagtaildocs/folder/add.html', {
+            'help_text': '',
+            'parent_folder': parent_folder,
+            'form': form,
+        })
 
 
 @permission_checker.require('change')
 def edit(request, folder_id):
-    DocumentFolder = get_folder_model()
-    DocumentFolderForm = get_folder_form(DocumentFolder)
-
     folder = get_object_or_404(DocumentFolder, id=folder_id)
 
     if request.method == 'POST':
@@ -85,15 +76,27 @@ def edit(request, folder_id):
         form = DocumentFolderForm(request.POST)
 
         if form.is_valid():
-            # TODO - Check for clashing filenames
-            # Save folder
+
             folder.title = form.cleaned_data['title']
+
+            try:
+                # Check if the folder is present in the DB or physically present in the OS
+                folder.validate_folder()
+            except ValidationError as e:
+                form._errors['title'] = e.message
+                return render(request, 'wagtaildocs/folder/edit.html', {
+                    'error_message': 'Error adding folder',
+                    'help_text': '',
+                    'form': form,
+                })
+
             folder.save()
 
             # Success! Send back to index or document specific folder
             response = redirect('wagtaildocs:index')
             response['Location'] += '?folder={0}'.format(folder.id)
             return response
+
         else:
             # Validation error
             return render(request, 'wagtaildocs/folder/edit.html', {
@@ -113,26 +116,9 @@ def edit(request, folder_id):
 
 @permission_checker.require('change')
 def delete(request, folder_id):
-    Document = get_document_model()
-    DocumentFolder = get_folder_model()
     folder = get_object_or_404(DocumentFolder, id=folder_id)
 
-    # Make Sure folder contains no documents
-    documents = Document.objects.filter(folder=folder)
-
-    if documents.count() > 0:
-        error = True
-        error_text = "Cannot delete folder containing documents"
-    else:
-        error = False
-        error_text = ""
-
-    # Make sure folder contains no sub folders
-    if not error and DocumentFolder.objects.filter(folder=folder).count() > 0:
-        error = True
-        error_text = "Cannot delete folder containing subfolders"
-
-    if not error and request.method == 'POST':
+    if request.method == 'POST':
         # POST if confirmation of delete
 
         # try find a parent folder
@@ -148,8 +134,5 @@ def delete(request, folder_id):
         return response
 
     return render(request, 'wagtaildocs/folder/confirm_delete.html', {
-        'error': error,
-        'error_text': error_text,
         'folder': folder,
-        # 'form': form,
     })
